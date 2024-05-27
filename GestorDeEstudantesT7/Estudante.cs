a3987e645790f690f5b68acddfa5d8005d980dd9
mümu <diasmurillo05@gmail.com>
2024-05-27 15:16:41 -0300

    27/05
    Everton.

--- GestorDeEstudantesT7/Estudante.cs
+++ GestorDeEstudantesT7/Estudante.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,7 @@ namespace GestorDeEstudantesT7
 
         public bool inserirEstudante(string nome, string sobrenome, DateTime nascimento, string telefone, string genero, string endereco, MemoryStream foto)
         {
-            MySqlCommand comando = new MySqlCommand("INSERT INTO `estudantes`(`id`, `nome`, `sobrenome`, `nascimento`, `genero`, `telefone`, `endereco`, `foto`) VALUES (@nome,@sobrenome,@nascimento,@genero,@telefone,@endereco,@foto)", meuBancoDeDados.getConexao);
+            MySqlCommand comando = new MySqlCommand("INSERT INTO `estudantes`(`nome`, `sobrenome`, `nascimento`, `genero`, `telefone`, `endereco`, `foto`) VALUES (@nome,@sobrenome,@nascimento,@genero,@telefone,@endereco,@foto)", meuBancoDeDados.getConexao);
 
             comando.Parameters.Add("@nome", MySqlDbType.VarChar).Value = nome;
             comando.Parameters.Add("@sobrenome", MySqlDbType.VarChar).Value = sobrenome;
@@ -22,20 +23,31 @@ namespace GestorDeEstudantesT7
             comando.Parameters.Add("@genero", MySqlDbType.VarChar).Value = genero;
             comando.Parameters.Add("@telefone", MySqlDbType.VarChar).Value = telefone;
             comando.Parameters.Add("@endereco", MySqlDbType.Text).Value = endereco;
-            comando.Parameters.Add("@foto", MySqlDbType.LongBlob).Value = foto;
+            comando.Parameters.Add("@foto", MySqlDbType.LongBlob).Value = foto.ToArray();
 
             meuBancoDeDados.abrirConexao();
 
             if (comando.ExecuteNonQuery() == 1)
             {
                 meuBancoDeDados.fecharConexao();
-                return true;
+                return true;    
             }
             else
             {
                 meuBancoDeDados.fecharConexao();
                 return false;
             }
+        }
+
+        public DataTable  getEstudantes (MySqlCommand comando)
+        {
+            comando.Connection = meuBancoDeDados.getConexao;
+
+            MySqlDataAdapter adaptador = new MySqlDataAdapter(comando);
+            DataTable tabelaDeDados = new DataTable();
+            adaptador.Fill(tabelaDeDados);
+
+            return tabelaDeDados;
         }
     }
 }
---
+++ GestorDeEstudantesT7/FormListarEstudantes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace GestorDeEstudantesT7
+{
+    public partial class FormListarEstudantes : Form
+    {
+        public FormListarEstudantes()
+        {
+            InitializeComponent();
+        }
+
+        Estudante estudante = new Estudante();
+
+        private void FormListarEstudantes_Load(object sender, EventArgs e)
+        {
+            MySqlCommand comando = new MySqlCommand("SELECT * FROM `estudantes`");
+
+            dataGridViewListaDeAlunos.ReadOnly = true;
+
+            DataGridViewImageColumn colunaDeFotos = new DataGridViewImageColumn();
+
+            dataGridViewListaDeAlunos.RowTemplate.Height = 80;
+
+            dataGridViewListaDeAlunos.DataSource = estudante.getEstudantes(comando);
+
+            colunaDeFotos = (DataGridViewImageColumn)dataGridViewListaDeAlunos.Columns[7];
+            colunaDeFotos.ImageLayout = DataGridViewImageCellLayout.Stretch;
+
+            dataGridViewListaDeAlunos.AllowUserToAddRows = false;
+        }
+    }
+}
